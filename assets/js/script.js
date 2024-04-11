f92a843715cd0b4820b100e4a3c52b94cc422055
Joe Ortiz <Se_sema_95@hotmail.com>
2024-04-11 11:29:32 +0000

    Installed prettier extension so document format and layout looks better

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,50 @@
 //console.log("Hello from the console!");
 
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
+  let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-            }
-        })
-    }
-})
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);
+      }
+    });
+  }
+});
 
 /**
  * Main game's "loop". Fn gets called when script first loads,
  * & after users' answer's processed.
  */
 function runGame() {
-
-    //Creates 2 random numbers between 1 & 25
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
+  //Creates 2 random numbers between 1 & 25
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
-function checkAnswer() {
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
+function checkAnswer() {}
 
-}
+/**
+ * Gets the operands (numbers) & the operator (plus, minus etc.)
+ * directly from the dom & returns corect answer.
+ */
+function calculateCorrectAnswer() {}
 
-function calculateCorrectAnswer() {
+function incrementScore() {}
 
-}
+function incrementWrongAnswer() {}
 
-function incrementScore() {
+function displayAdditionQuestion() {}
 
-}
+function displaySubtractQuestion() {}
 
-function incrementWrongAnswer() {
-
-}
-
-function displayAdditionQuestion() {
-
-}
-
-function displaySubtractQuestion() {
-
-}
-
-function displayMultiplyQuestion() {
-
-}
+function displayMultiplyQuestion() {}
 
 //function displayDivisionQuestion() {}
-
