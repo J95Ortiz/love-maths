9307305d09412fd76d2f9aedbd48287782c7ec20
Joe Ortiz <Se_sema_95@hotmail.com>
2024-04-11 10:04:08 +0000

    added functions to JS and imported code to CSS file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,58 @@
 //console.log("Hello from the console!");
 
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+/**
+ * Main game's "loop". Fn gets called when script first loads,
+ * & after users' answer's processed.
+ */
+function runGame() {
+
+    //Creates 2 random numbers between 1 & 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+//function displayDivisionQuestion() {}
+
