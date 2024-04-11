f1eba8083587cedd48ca80ddf1476b3e2ecd843c
Joe Ortiz <Se_sema_95@hotmail.com>
2024-04-11 15:07:38 +0000

    added code for subtraction & multiply games. Added code for better interactivity also.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  document
+    .getElementById("answer-box")
+    .addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        checkAnswer();
+      }
+    });
+
   runGame("addition");
 });
 
@@ -22,6 +30,12 @@ document.addEventListener("DOMContentLoaded", function () {
  * & after users' answer's processed.
  */
 function runGame(gameType) {
+  //Wipes the previous answer from the answer box when a new question is loaded.
+  document.getElementById("answer-box").value = "";
+
+  //Sets the focus of the page on the answer box so user doesn't have to click on it each time.
+  document.getElementById("answer-box").focus();
+
   //Creates 2 random numbers between 1 & 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
@@ -31,7 +45,7 @@ function runGame(gameType) {
   } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
   } else if (gameType === "subtract") {
-    displayMultiplyQuestion(num1, num2);
+    displaySubtractQuestion(num1, num2);
   } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -73,6 +87,8 @@ function calculateCorrectAnswer() {
     return [operand1 + operand2, "addition"];
   } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
+  } else if (operator === "-") {
+    return [operand1 - operand2, "subtract"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -101,7 +117,13 @@ function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {}
+function displaySubtractQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent =
+    operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent =
+    operand1 > operand2 ? operand2 : operand1;
+  document.getElementById("operator").textContent = "-";
+}
 
 function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
